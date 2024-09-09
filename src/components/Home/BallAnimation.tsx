bd2acc054cf18216e641415bb76f262986bc6eb4
aprilmarch-chaebeen <chaebeen86@gmail.com>
2024-09-09 12:46:54 +0900

    Chore: Home journey 공 애니메이션 성능 이슈로 잠시 제거

--- src/components/Home/BallAnimation.tsx
+++ src/components/Home/BallAnimation.tsx
@@ -1,56 +1,56 @@
-import identitysrc from '../../assets/svg/Identity.svg';
-import besrc from '../../assets/svg/brand_experience.svg';
-import trendsrc from '../../assets/svg/trend_insight.svg';
-import websrc from '../../assets/svg/web.svg';
-import uxsrc from '../../assets/svg/ux.svg';
-import designsrc from '../../assets/svg/creative_design.svg';
-import videosrc from '../../assets/svg/video_production.svg';
-import marketingsrc from '../../assets/svg/digital_marketing.svg';
-import {useEffect, useRef, useState} from 'react';
+// import identitysrc from '../../assets/svg/Identity.svg';
+// import besrc from '../../assets/svg/brand_experience.svg';
+// import trendsrc from '../../assets/svg/trend_insight.svg';
+// import websrc from '../../assets/svg/web.svg';
+// import uxsrc from '../../assets/svg/ux.svg';
+// import designsrc from '../../assets/svg/creative_design.svg';
+// import videosrc from '../../assets/svg/video_production.svg';
+// import marketingsrc from '../../assets/svg/digital_marketing.svg';
+// import {useEffect, useRef, useState} from 'react';
 import styled from 'styled-components';
 
-const getPosition = (elapsedTime: number, h: number, k: number) => {
-  const a = (4 * k) / Math.pow(h * 2, 2);
-  const ypos = a * Math.pow(((elapsedTime + h) % (h * 2)) - h, 2);
+// const getPosition = (elapsedTime: number, h: number, k: number) => {
+//   const a = (4 * k) / Math.pow(h * 2, 2);
+//   const ypos = a * Math.pow(((elapsedTime + h) % (h * 2)) - h, 2);
 
-  return ypos;
-};
+//   return ypos;
+// };
 
-function useQuadBounce({duration = 1200, start = 0, end = 10} = {}) {
-  const timeStart = useRef(Date.now());
-  const [value, setValue] = useState(start);
+// function useQuadBounce({duration = 1200, start = 0, end = 10} = {}) {
+//   const timeStart = useRef(Date.now());
+//   const [value, setValue] = useState(start);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const time = Date.now() - timeStart.current;
-      setValue(start + getPosition(time, duration / 2, end - start));
-    }, 1);
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       const time = Date.now() - timeStart.current;
+//       setValue(start + getPosition(time, duration / 2, end - start));
+//     }, 1);
 
-    return () => clearInterval(interval);
-  }, []);
+//     return () => clearInterval(interval);
+//   }, []);
 
-  return value;
-}
+//   return value;
+// }
 
-const Circlesbgimg = [identitysrc, besrc, trendsrc, websrc, uxsrc, designsrc, videosrc, marketingsrc];
+// const Circlesbgimg = [identitysrc, besrc, trendsrc, websrc, uxsrc, designsrc, videosrc, marketingsrc];
 
 function BallAnimation() {
-  const ballSettings = [
-    {duration: 2200, start: 0, end: 20},
-    {duration: 1900, start: 5, end: 20},
-    {duration: 1500, start: 8, end: 20},
-    {duration: 1800, start: 7, end: 20},
-    {duration: 2000, start: 5, end: 20},
-    {duration: 1900, start: 6, end: 20},
-    {duration: 2200, start: 2, end: 20},
-    {duration: 1700, start: 3, end: 20},
-  ];
+  // const ballSettings = [
+  //   {duration: 2200, start: 0, end: 20},
+  //   {duration: 1900, start: 5, end: 20},
+  //   {duration: 1500, start: 8, end: 20},
+  //   {duration: 1800, start: 7, end: 20},
+  //   {duration: 2000, start: 5, end: 20},
+  //   {duration: 1900, start: 6, end: 20},
+  //   {duration: 2200, start: 2, end: 20},
+  //   {duration: 1700, start: 3, end: 20},
+  // ];
 
   return (
     <BallContainer>
-      {ballSettings.map((settings, index) => (
+      {/* {ballSettings.map((settings, index) => (
         <Ball key={index} src={Circlesbgimg[index]} alt={`ball ${index}`} $y={useQuadBounce(settings)} />
-      ))}
+      ))} */}
     </BallContainer>
   );
 }
@@ -63,8 +63,12 @@ const BallContainer = styled.div`
   padding: 0 7vw;
 `;
 
-const Ball = styled.img<{$y: number}>`
-  width: 9.5vw;
-  margin-right: 2vw;
-  transform: translate3d(0, ${(p) => p.$y}vw, 0);
-`;
+// const Ball = styled.img.attrs<Position>((p: number) => ({
+//   style: {
+//     transform: `translate3d(0, ${p.$y}vw, 0)`
+//   },
+// }))<Position>`
+//   width: 9.5vw;
+//   margin-right: 2vw;
+
+// `;
