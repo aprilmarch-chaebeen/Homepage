77fbb1de84fbfe9792d5db2ea2f3f502b25e0244
aprilmarch-chaebeen <chaebeen86@gmail.com>
2024-09-10 15:16:53 +0900

    Style: Home journey 스타일 전반적으로 수정

--- src/components/Home/BallAnimation.tsx
+++ src/components/Home/BallAnimation.tsx
@@ -1,56 +1,24 @@
-// import identitysrc from '../../assets/svg/Identity.svg';
-// import besrc from '../../assets/svg/brand_experience.svg';
-// import trendsrc from '../../assets/svg/trend_insight.svg';
-// import websrc from '../../assets/svg/web.svg';
-// import uxsrc from '../../assets/svg/ux.svg';
-// import designsrc from '../../assets/svg/creative_design.svg';
-// import videosrc from '../../assets/svg/video_production.svg';
-// import marketingsrc from '../../assets/svg/digital_marketing.svg';
-// import {useEffect, useRef, useState} from 'react';
+import identitysrc from '../../assets/svg/identity.svg';
+import besrc from '../../assets/svg/brand_experience.svg';
+import trendsrc from '../../assets/svg/trend_insight.svg';
+import websrc from '../../assets/svg/web_build.svg';
+import uxsrc from '../../assets/svg/ux.svg';
+import designsrc from '../../assets/svg/creative_design.svg';
+import videosrc from '../../assets/svg/video.svg';
+import digitalsrc from '../../assets/svg/marketing.svg';
 import styled from 'styled-components';
 
-// const getPosition = (elapsedTime: number, h: number, k: number) => {
-//   const a = (4 * k) / Math.pow(h * 2, 2);
-//   const ypos = a * Math.pow(((elapsedTime + h) % (h * 2)) - h, 2);
-
-//   return ypos;
-// };
-
-// function useQuadBounce({duration = 1200, start = 0, end = 10} = {}) {
-//   const timeStart = useRef(Date.now());
-//   const [value, setValue] = useState(start);
-
-//   useEffect(() => {
-//     const interval = setInterval(() => {
-//       const time = Date.now() - timeStart.current;
-//       setValue(start + getPosition(time, duration / 2, end - start));
-//     }, 1);
-
-//     return () => clearInterval(interval);
-//   }, []);
-
-//   return value;
-// }
-
-// const Circlesbgimg = [identitysrc, besrc, trendsrc, websrc, uxsrc, designsrc, videosrc, marketingsrc];
-
 function BallAnimation() {
-  // const ballSettings = [
-  //   {duration: 2200, start: 0, end: 20},
-  //   {duration: 1900, start: 5, end: 20},
-  //   {duration: 1500, start: 8, end: 20},
-  //   {duration: 1800, start: 7, end: 20},
-  //   {duration: 2000, start: 5, end: 20},
-  //   {duration: 1900, start: 6, end: 20},
-  //   {duration: 2200, start: 2, end: 20},
-  //   {duration: 1700, start: 3, end: 20},
-  // ];
-
   return (
     <BallContainer>
-      {/* {ballSettings.map((settings, index) => (
-        <Ball key={index} src={Circlesbgimg[index]} alt={`ball ${index}`} $y={useQuadBounce(settings)} />
-      ))} */}
+      <Ball src={uxsrc} alt="ux ball" $top={5} $left={51} />
+      <Ball src={identitysrc} alt="identity ball" $top={20} $left={17} />
+      <Ball src={trendsrc} alt="trend & insights ball" $top={27} $left={33} />
+      <Ball src={videosrc} alt="video production ball" $top={24} $left={60} />
+      <Ball src={digitalsrc} alt="digital marketing ball" $top={43} $left={70} />
+      <Ball src={websrc} alt="web build ball" $top={49} $left={43} />
+      <Ball src={besrc} alt="be ball" $top={60} $left={18} />
+      <Ball src={designsrc} alt="creative design ball" $top={62} $left={60} />
     </BallContainer>
   );
 }
@@ -61,14 +29,12 @@ const BallContainer = styled.div`
   display: flex;
   justify-content: center;
   padding: 0 7vw;
+  position: relative;
 `;
 
-// const Ball = styled.img.attrs<Position>((p: number) => ({
-//   style: {
-//     transform: `translate3d(0, ${p.$y}vw, 0)`
-//   },
-// }))<Position>`
-//   width: 9.5vw;
-//   margin-right: 2vw;
-
-// `;
+const Ball = styled.img<{$top: number; $left: number}>`
+  width: 14vw;
+  position: absolute;
+  top: ${(p) => p.$top}vw;
+  left: ${(p) => p.$left}vw;
+`;
--- src/components/Home/HomeJourney.tsx
+++ src/components/Home/HomeJourney.tsx
@@ -1,43 +1,12 @@
-// import Marquee from 'react-fast-marquee';
 import styled from 'styled-components';
-// import {motion} from 'framer-motion';
-// import {useEffect, useRef} from 'react';
 import BallAnimation from './BallAnimation';
-
-// const transitionValues = {
-//   duration: 2,
-//   repeat: Infinity,
-//   ease: 'easeOut',
-// };
 
 function HomeJourney() {
   return (
     <JourneySection>
       <BigText>Work Journey Map.</BigText>
       <BallAnimation />
-      {/* <MarqueeDiv autoFill={true} direction="right" speed={20}>
-        <motion.div animate={{y: [30, 0, 30]}} transition={transitionValues}>
-          <WorkImg src={identitysrc} alt="identity" />
-        </motion.div>
-        <motion.div animate={{y: [0, 30, 0]}} transition={transitionValues}>
-          <WorkImg src={besrc} alt="be" />
-        </motion.div>
-        <motion.div animate={{y: [30, 0, 30]}} transition={transitionValues}>
-          <WorkImg src={trendsrc} alt="trend" />
-        </motion.div>
-        <motion.div animate={{y: [0, 30, 0]}} transition={transitionValues}>
-          <WorkImg src={websrc} alt="web" />
-        </motion.div>
-        <motion.div animate={{y: [30, 0, 30]}} transition={transitionValues}>
-          <WorkImg src={uxsrc} alt="ux" />
-        </motion.div>
-        <motion.div animate={{y: [0, 30, 0]}} transition={transitionValues}>
-          <WorkImg src={designsrc} alt="design" />
-        </motion.div>
-        <motion.div animate={{y: [30, 0, 30]}} transition={transitionValues}>
-          <WorkImg src={videosrc} alt="video" />
-        </motion.div>
-      </MarqueeDiv> */}
+      <Line />
     </JourneySection>
   );
 }
@@ -48,7 +17,19 @@ const JourneySection = styled.section`
   background-color: #000;
   color: #fff;
   padding: 13vw 0;
-  height: 40vw;
+  height: 90vw;
+  position: relative;
+  z-index: -2;
+`;
+
+const Line = styled.hr`
+  border: 0;
+  background-color: #d2ff52;
+  width: 100vw;
+  position: absolute;
+  z-index: -1;
+  height: 0.1vw;
+  top: 58%;
 `;
 
 const BigText = styled.h3`
@@ -59,12 +40,3 @@ const BigText = styled.h3`
   margin-bottom: 5vw;
   font-size: 3.4vw;
 `;
-
-// const MarqueeDiv = styled(Marquee)`
-//   height: 25vw;
-// `;
-
-// const WorkImg = styled.img`
-//   margin-right: 24px;
-//   width: 16vw;
-// `;
