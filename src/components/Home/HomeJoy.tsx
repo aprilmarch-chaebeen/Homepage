7ca009c1452c1e00e59aed4dd05a858a547cbcf4
aprilmarch-chaebeen <chaebeen86@gmail.com>
2024-09-10 19:08:59 +0900

    Chore: Home joy 스크롤 애니메이션 추가

--- src/components/Home/HomeJoy.tsx
+++ src/components/Home/HomeJoy.tsx
@@ -5,24 +5,57 @@ import joy1gsrc from '../../assets/svg/joy1_g.svg';
 import joy2gsrc from '../../assets/svg/joy2_g.svg';
 import joy3gsrc from '../../assets/svg/joy3_g.svg';
 import joy4gsrc from '../../assets/svg/joy4_g.svg';
+import joy1bsrc from '../../assets/svg/joy1_b.svg';
+import joy2bsrc from '../../assets/svg/joy2_b.svg';
+import joy3bsrc from '../../assets/svg/joy3_b.svg';
 import circlesrc from '../../assets/svg/small_circle.svg';
 import circlebacksrc from '../../assets/svg/circle_back.svg';
 import contactsrc from '../../assets/svg/contact_us.svg';
+import {useRef} from 'react';
+import gsap from 'gsap';
+import {useGSAP} from '@gsap/react';
+import {ScrollTrigger} from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 function HomeJoy() {
+  const svgsRef = useRef<HTMLImageElement[] | null[]>([]);
+
+  useGSAP(
+    () => {
+      svgsRef.current.forEach((svg) => {
+        gsap
+          .timeline({
+            opacity: 1,
+            scrollTrigger: {
+              trigger: svg,
+              start: 'top top',
+
+              scrub: true,
+            },
+          })
+          .from(svg, {opacity: 0, transformOrigin: 'left'});
+      });
+    },
+    {scope: svgsRef}
+  );
+
   return (
     <Section>
       <Star src={starsrc} alt="star img" />
       <Container $direct="space-between">
         <Rect src={rectsrc} alt="rectangle img" />
         <TextImg src={joy1gsrc} alt="branding text img" />
+        <BlueTextImg src={joy1bsrc} alt="branding blue text img" ref={(el) => (svgsRef.current[0] = el)} />
       </Container>
       <Container $direct="space-between">
         <Circle src={circlesrc} alt="circle img" />
         <TextImg src={joy2gsrc} alt="uxui text img" />
+        <BlueTextImg src={joy2bsrc} alt="branding blue text img" ref={(el) => (svgsRef.current[1] = el)} />
       </Container>
       <Container $direct="end">
         <TextImg src={joy3gsrc} alt="digital, it text img" />
+        <BlueTextImg src={joy3bsrc} alt="branding blue text img" ref={(el) => (svgsRef.current[2] = el)} />
       </Container>
       <Container $direct="space-evenly">
         <TextImg src={joy4gsrc} alt="aprilmarch text img" />
@@ -89,6 +122,13 @@ const Rect = styled.img`
 
 const TextImg = styled.img`
   height: 13vw;
+  position: relative;
+`;
+
+const BlueTextImg = styled.img`
+  height: 13vw;
+  position: absolute;
+  right: 0;
 `;
 
 const Circle = styled.img`
