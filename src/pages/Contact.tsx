e69e0448539a36827477286854d4def04e1cbc5d
aprilmarch-chaebeen <chaebeen86@gmail.com>
2024-10-08 09:07:12 +0900

    Chore: Contact 폼 부분 수정 위해 이미지로 대체

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,78 +1,86 @@
-import {useState} from 'react';
+// import {useState} from 'react';
 import styled from 'styled-components';
-import iconsrc from '../assets/svg/contact_icon.svg';
+// import iconsrc from '../assets/svg/contact_icon.svg';
 import HomeFooter from '../components/Home/HomeFooter';
+import statuesrc from '../assets/svg/contact_statue.svg';
 
 function Contact() {
-  const [formData, setFormData] = useState({
-    company: '',
-    phone: '',
-    name: '',
-    email: '',
-  });
-  const [errors, setErrors] = useState({
-    company: '',
-    phone: '',
-    name: '',
-    email: '',
-  });
+  //   const [formData, setFormData] = useState({
+  //     company: '',
+  //     phone: '',
+  //     name: '',
+  //     email: '',
+  //   });
+  //   const [errors, setErrors] = useState({
+  //     company: '',
+  //     phone: '',
+  //     name: '',
+  //     email: '',
+  //   });
 
-  const validateEmail = (email: string) => {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regex.test(email);
-  };
+  //   const validateEmail = (email: string) => {
+  //     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  //     return regex.test(email);
+  //   };
 
-  const validatePhone = (phone: string) => {
-    const regex = /^\d{2,3}-\d{3,4}-\d{4}$/; // e.g., 010-1234-5678
-    return regex.test(phone);
-  };
+  //   const validatePhone = (phone: string) => {
+  //     const regex = /^\d{2,3}-\d{3,4}-\d{4}$/; // e.g., 010-1234-5678
+  //     return regex.test(phone);
+  //   };
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const {name, value} = e.target;
-    setFormData({...formData, [name]: value});
+  //   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //     const {name, value} = e.target;
+  //     setFormData({...formData, [name]: value});
 
-    if (name === 'email' && !validateEmail(value)) {
-      setErrors({...errors, email: '유효한 이메일 주소를 입력하세요.'});
-    } else if (name === 'phone' && !validatePhone(value)) {
-      setErrors({...errors, phone: '유효한 전화번호를 입력하세요. (예: 010-1234-5678)'});
-    } else {
-      setErrors({...errors, [name]: ''});
-    }
-  };
+  //     if (name === 'email' && !validateEmail(value)) {
+  //       setErrors({...errors, email: '유효한 이메일 주소를 입력하세요.'});
+  //     } else if (name === 'phone' && !validatePhone(value)) {
+  //       setErrors({...errors, phone: '유효한 전화번호를 입력하세요. (예: 010-1234-5678)'});
+  //     } else {
+  //       setErrors({...errors, [name]: ''});
+  //     }
+  //   };
 
-  const handleSubmitClick = (e: React.FormEvent) => {
-    e.preventDefault();
+  //   const handleSubmitClick = (e: React.FormEvent) => {
+  //     e.preventDefault();
 
-    let hasErrors = false;
-    const newErrors = {...errors};
+  //     let hasErrors = false;
+  //     const newErrors = {...errors};
 
-    if (!formData.company) {
-      newErrors.company = '회사명을 입력하세요.';
-      hasErrors = true;
-    }
+  //     if (!formData.company) {
+  //       newErrors.company = '회사명을 입력하세요.';
+  //       hasErrors = true;
+  //     }
 
-    if (!formData.phone || !validatePhone(formData.phone)) {
-      newErrors.phone = '유효한 전화번호를 입력하세요. (예: 010-1234-5678)';
-      hasErrors = true;
-    }
+  //     if (!formData.phone || !validatePhone(formData.phone)) {
+  //       newErrors.phone = '유효한 전화번호를 입력하세요. (예: 010-1234-5678)';
+  //       hasErrors = true;
+  //     }
 
-    if (!formData.name) {
-      newErrors.name = '담당자명을 입력하세요.';
-      hasErrors = true;
-    }
+  //     if (!formData.name) {
+  //       newErrors.name = '담당자명을 입력하세요.';
+  //       hasErrors = true;
+  //     }
 
-    if (!formData.email || !validateEmail(formData.email)) {
-      newErrors.email = '유효한 이메일 주소를 입력하세요.';
-      hasErrors = true;
-    }
+  //     if (!formData.email || !validateEmail(formData.email)) {
+  //       newErrors.email = '유효한 이메일 주소를 입력하세요.';
+  //       hasErrors = true;
+  //     }
 
-    setErrors(newErrors);
+  //     setErrors(newErrors);
 
-    if (!hasErrors) {
-      // 모든 유효성 검사를 통과한 경우 폼 제출
-      document.getElementById('contactForm')?.onsubmit;
-    }
-  };
+  //     if (!hasErrors) {
+  //       // 모든 유효성 검사를 통과한 경우 폼 제출
+  //       alert('문의가 완료되었습니다.');
+  //       setFormData({
+  //         company: '',
+  //         phone: '',
+  //         name: '',
+  //         email: '',
+  //       });
+  //       document.getElementById('contactForm')?.onsubmit;
+  //     }
+  //   };
 
   return (
     <section>
@@ -93,14 +101,9 @@ function Contact() {
             </p>
           </TextBox>
         </TextContainer>
-        <FormBox>
-          <form
-            id="contactForm"
-            role="form"
-            method="POST"
-            action="https://script.google.com/macros/s/AKfycbxdT1GPEj146Oygn33ljlp64IXZhE9c_mdApb-hZgOLOZ_0BsCNrOoD5Ji8WQnygpHE/exec"
-            target="formDisplay"
-          >
+        <Statue src={statuesrc} alt="contact statue img" />
+        {/* <FormBox>
+          <form id="contactForm" role="form" method="POST" action={process.env.HTML_CONTACT_FORM_WEB_APP_URL} target="formDisplay">
             <Icon src={iconsrc} alt="contact icon" />
             <InputContainer>
               <Input type="text" name="company" placeholder="회사명" value={formData.company} onChange={handleChange} required />
@@ -131,7 +134,7 @@ function Contact() {
             </SubmitContainer>
           </form>
           <iframe name="formDisplay" style={{display: 'none'}}></iframe>
-        </FormBox>
+        </FormBox> */}
       </Container>
       <HomeFooter />
     </section>
@@ -182,45 +185,50 @@ const TextBlack = styled.span`
   color: #131313;
 `;
 
-const FormBox = styled.div`
-  margin-top: 10vw;
-`;
+// const FormBox = styled.div`
+//   margin-top: 10vw;
+// `;
 
-const Icon = styled.img`
-  margin-bottom: 1.5vw;
-  width: 0.8vw;
-`;
+// const Icon = styled.img`
+//   margin-bottom: 1.5vw;
+//   width: 0.8vw;
+// `;
 
-const InputContainer = styled.div`
-  margin-bottom: 1.5vw;
-`;
+// const InputContainer = styled.div`
+//   margin-bottom: 1.5vw;
+// `;
 
-const Input = styled.input`
-  font-family: 'Pretendard-Regular', sans-serif;
-  width: 30vw;
-  height: 2vw;
-  padding-bottom: 0.4vw;
-  font-size: 1vw;
-  border: none;
-  border-bottom: 0.05vw solid #ccc;
-  color: #111;
-  cursor: pointer;
-`;
+// const Input = styled.input`
+//   font-family: 'Pretendard-Regular', sans-serif;
+//   width: 30vw;
+//   height: 2vw;
+//   padding-bottom: 0.4vw;
+//   font-size: 1vw;
+//   border: none;
+//   border-bottom: 0.05vw solid #ccc;
+//   color: #111;
+//   cursor: pointer;
+// `;
 
-const SubmitContainer = styled.div`
-  text-align: right;
-`;
+// const SubmitContainer = styled.div`
+//   text-align: right;
+// `;
 
-const Submit = styled.input`
-  background: #fff;
-  border: 0.05vw solid #222;
-  color: #222;
-  font-size: 1vw;
-  padding: 0.7vw 2.5vw;
-`;
+// const Submit = styled.input`
+//   background: #fff;
+//   border: 0.05vw solid #222;
+//   color: #222;
+//   font-size: 1vw;
+//   padding: 0.7vw 2.5vw;
+//   cursor: pointer;
+// `;
 
-const ErrorMessage = styled.p`
-  color: red;
-  font-size: 0.8vw;
-  margin: 0.5vw 0;
+// const ErrorMessage = styled.p`
+//   color: red;
+//   font-size: 0.8vw;
+//   margin: 0.5vw 0;
+// `;
+
+const Statue = styled.img`
+  width: 19vw;
 `;
